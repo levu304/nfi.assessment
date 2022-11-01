7f2dd71e657a8c06d5d4bd088bbe7b7421ec02dd
levu304 <ln-anhvu@vn.pasonatech.co.jp>
2022-11-01 14:15:56 +0700

    implement assessment

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(true);
+  const [account, setAccount] = useState<string | null>(null);
+  const [network, setNetwork] = useState<string | null>(null);
+
+  useEffect(() => {
+    const ethereum = window.ethereum;
+
+    const init = async () => {
+      const chainId = await ethereum.request({ method: "eth_chainId" });
+      setNetwork(chainId);
+      setLoading(false);
+    };
+
+    init();
+
+    const handleChainChanged = (_chainId: string) => {
+      setNetwork(_chainId);
+    };
+
+    const handleAccountsChanged = (accounts: string[]) => {
+      if (accounts[0] !== account) {
+        setAccount(accounts[0]);
+      }
+    };
+
+    ethereum.on("chainChanged", handleChainChanged);
+    ethereum.on("accountsChanged", handleAccountsChanged);
+  }, []);
+
+  const connect = async () => {
+    const ethereum = window.ethereum;
+    const accounts: string[] = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    if (accounts.length !== 0) {
+      setAccount(accounts[0]);
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p>Network ID</p>
+          <p
+            style={{
+              background: "#ccc",
+              padding: 10,
+              color: "black",
+            }}
+          >
+            {network}
+          </p>
+          {account ? (
+            <>
+              <p>Account</p>
+              <p
+                style={{
+                  background: "#ccc",
+                  padding: 10,
+                  color: "black",
+                }}
+              >
+                {account}
+              </p>
+            </>
+          ) : (
+            <button onClick={connect}>Connect</button>
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
